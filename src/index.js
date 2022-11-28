fd0c9b41da208db21d597005ecce0d1bdb52b523
Stfrancoc <stfrancoc.com>
2022-11-28 13:08:28 -0500

    update the process of second carrusel

---
+++ src/index.js
@@ -0,0 +1,2 @@
+let prev = document.querySelector(".prev").addEventListener("click",()=>{console.log("sisas")})
+let next = document.querySelector(".next").addEventListener("click",()=>{console.log("sisas")})
