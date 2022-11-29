5b62c36548292f6eae29b299318b147eb2136386
Stfrancoc <stfrancoc.com>
2022-11-28 20:49:13 -0500

    the move of the first carrusel succesfull only have to put de infomation on the images

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
-let prev = document.querySelector(".prev").addEventListener("click",()=>{console.log("sisas")})
-let next = document.querySelector(".next").addEventListener("click",()=>{console.log("sisas")})
+document.addEventListener("DOMContentLoaded",()=>{
+
+    let prev = document.querySelector(".prev")
+    let next = document.querySelector(".next")
+    let carucel = document.querySelector(".caruc1")
+
+    prev.addEventListener("click",()=>{
+        
+        let cont = 0;
+        const inter = setInterval(() => {
+            cont++;
+            carucel.scrollLeft = carucel.scrollLeft - 1;
+            console.log(cont)    
+            if(cont == 350){
+                clearInterval(inter);
+            }
+        }, 1);
+        
+        //carucel.scrollLeft = carucel.scrollLeft - 350;
+    })
+
+    next.addEventListener("click",()=>{
+        let cont = 0;
+        const inter = setInterval(() => {
+            cont++;
+            carucel.scrollLeft = carucel.scrollLeft + 1;
+            console.log(cont)    
+            if(cont == 350){
+                clearInterval(inter);
+            }
+        }, 1);
+    })
+
+})
+
+
+
