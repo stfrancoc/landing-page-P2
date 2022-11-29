03f2c0412a45b4247eb309762425970e37e2824c
stfrancoc <stfrancoc@gmail.com>
2022-11-29 17:25:56 -0500

    finish the cards products

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     let next = document.querySelector(".next")
     let carucel = document.querySelector(".caruc1")
     let acumCaru = 0
+    
 
     prev.addEventListener("click",()=>{
         if(acumCaru == 1){
@@ -14,16 +15,13 @@ document.addEventListener("DOMContentLoaded",()=>{
                 if(cont == 400){
                     clearInterval(inter);
                     acumCaru--;
-                    console.log(acumCaru);
                 }
     
             }, 0.25);
         }
         else{
-            console.log("not do")
             acumCaru = 0
         }
-        //carucel.scrollLeft = carucel.scrollLeft - 350;
     })
 
     next.addEventListener("click",()=>{
@@ -35,15 +33,12 @@ document.addEventListener("DOMContentLoaded",()=>{
             if(cont == 385){
                 clearInterval(inter);
                 acumCaru++;
-                console.log(acumCaru);
             }
         }, 0.25);
     }else{
-        console.log("not do")
         acumCaru = 1
     }})
 
 })
-
 
 
