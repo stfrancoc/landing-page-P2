1a830e916d82fdbc5dc8ae57d08ef7d34358adf1
stfrancoc <stfrancoc@gmail.com>
2022-11-29 01:06:28 -0500

    finish the first carruseÃÃl with alll info

--- src/index.js
+++ src/index.js
@@ -3,33 +3,45 @@ document.addEventListener("DOMContentLoaded",()=>{
     let prev = document.querySelector(".prev")
     let next = document.querySelector(".next")
     let carucel = document.querySelector(".caruc1")
+    let acumCaru = 0
 
     prev.addEventListener("click",()=>{
-        
-        let cont = 0;
-        const inter = setInterval(() => {
-            cont++;
-            carucel.scrollLeft = carucel.scrollLeft - 1;
-            console.log(cont)    
-            if(cont == 350){
-                clearInterval(inter);
-            }
-        }, 1);
-        
+        if(acumCaru == 1){
+            let cont = 0;
+            const inter = setInterval(() => {
+                cont++;
+                carucel.scrollLeft = carucel.scrollLeft - 1;   
+                if(cont == 400){
+                    clearInterval(inter);
+                    acumCaru--;
+                    console.log(acumCaru);
+                }
+    
+            }, 0.25);
+        }
+        else{
+            console.log("not do")
+            acumCaru = 0
+        }
         //carucel.scrollLeft = carucel.scrollLeft - 350;
     })
 
     next.addEventListener("click",()=>{
+        if(acumCaru == 0){
         let cont = 0;
         const inter = setInterval(() => {
             cont++;
-            carucel.scrollLeft = carucel.scrollLeft + 1;
-            console.log(cont)    
-            if(cont == 350){
+            carucel.scrollLeft = carucel.scrollLeft + 1;    
+            if(cont == 385){
                 clearInterval(inter);
+                acumCaru++;
+                console.log(acumCaru);
             }
-        }, 1);
-    })
+        }, 0.25);
+    }else{
+        console.log("not do")
+        acumCaru = 1
+    }})
 
 })
 
